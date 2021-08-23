557f0c48bda34100467330b4dca229e67821c605
Thomah <5090230+Thomah@users.noreply.github.com>
2021-08-23 22:53:07 +0200

    Installation de gcloud, k9s, node, openjdk et slack

---
+++ run.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Variables globales
+export ANSIBLE_VERSION="4.3.0"
+
+# Synchronisation des dépôts APT 
+sudo apt -y update
+
+# Installation de python
+sudo apt -y install python3-pip
+
+# Création de l'alias python -> python3
+echo "alias python=python3" > ~/.bash_aliases
+source ~/.bash_aliases
+
+# Upgrade de pip
+pip3 install --upgrade pip
+
+# Installation de ansible
+pip3 install ansible===$ANSIBLE_VERSION
+
+# Application du nouveau fichier profile
+source ~/.profile
+
+# Lancement du playbook
+ansible-playbook ansible/playbook.yml -i ansible/inventory.yml --limit localhost
