8222c22f7070c2d6694f81503e2a6e6a01c0d79b
Thomah <5090230+Thomah@users.noreply.github.com>
2021-12-24 10:26:08 +0100

    Ajout de postgresql et dbeaver

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Variables globales
-export ANSIBLE_VERSION="4.3.0"
+export ANSIBLE_VERSION="5.1.0"
 
 # Synchronisation des dépôts APT 
 sudo apt -y update
@@ -21,6 +21,9 @@ pip3 install ansible===$ANSIBLE_VERSION
 
 # Application du nouveau fichier profile
 source ~/.profile
+
+# Installation des roles distants
+ansible-galaxy install -r ansible/requirements.yml
 
 # Lancement du playbook
 ansible-playbook ansible/playbook.yml -i ansible/inventory.yml --limit localhost
